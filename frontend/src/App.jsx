8b51fd18f178b4ef56d2984ce31931883dae1ba8
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-09-06 18:37:53 +0530

    Added resume route and toaster globally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UserProvider from "./context/UserContext";
 import Dasboard from "./pages/Dasboard";
+import EditResume from "./components/EditResume";
+import { Toaster } from "react-hot-toast"; // ✅ Import Toaster
 
 const App = () => {
   return (
     <UserProvider>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dasboard/>} />
-
-      </Routes>
+      <>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dasboard />} />
+          <Route path="/resume/:resumeId" element={<EditResume />} />
+        </Routes>
+        <Toaster position="top-right" reverseOrder={false} /> 
+      </>
     </UserProvider>
   );
 };
